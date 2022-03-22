cedbd9e20fa8be6901d1ab0d0ed5f7af602aac60
seungmin1592 <tmdals1592@naver.com>
2022-03-22 23:08:41 +0900

    시크릿코드 SpringBoot/JPA

--- SecretCode_SpringBoot-JPA/sample1/src/main/java/com/example/jpa/user/controller/ApiUserController.java
+++ SecretCode_SpringBoot-JPA/sample1/src/main/java/com/example/jpa/user/controller/ApiUserController.java
@@ -5,15 +5,17 @@ import com.example.jpa.notice.model.NoticeResponse;
 import com.example.jpa.notice.model.ResponseError;
 import com.example.jpa.notice.repository.NoticeRepository;
 import com.example.jpa.user.entity.User;
+import com.example.jpa.user.exception.ExistsEmailException;
+import com.example.jpa.user.exception.PasswordNotMatchException;
 import com.example.jpa.user.exception.UserNotFoundException;
-import com.example.jpa.user.model.UserInput;
-import com.example.jpa.user.model.UserResponse;
-import com.example.jpa.user.model.UserUpdate;
+import com.example.jpa.user.model.*;
 import com.example.jpa.user.repository.UserRepository;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
@@ -44,7 +46,7 @@ public class ApiUserController {
         return ResponseEntity.ok().build();
     }*/
 
-
+    /*
     @PostMapping("/api/user")
     public ResponseEntity<?> addUser(@RequestBody @Valid UserInput userInput, Errors errors){
 
@@ -67,7 +69,7 @@ public class ApiUserController {
 
         return ResponseEntity.ok().build();
     }
-
+    */
 
     @PutMapping("/api/user/{id}")
     public ResponseEntity<?> updateUser(@RequestBody @Valid UserUpdate userUpdate, @PathVariable long id, Errors errors){
@@ -121,5 +123,133 @@ public class ApiUserController {
 
         return noticeResponses;
     }
+    /*
+    @PostMapping("/api/user")
+    public ResponseEntity addUser(@RequestBody @Valid UserInput userInput, Errors errors){
+
+        List<ResponseError> responseErrorList = new ArrayList<>();
+        if(errors.hasErrors()){
+            errors.getAllErrors().stream().forEach((e) ->
+                    responseErrorList.add(ResponseError.of((FieldError)e))
+                    );
+            return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
+        }
+
+        if(userRepository.countByEmail(userInput.getEmail()) > 0) {
+            throw new ExistsEmailException("이미 가입한 이메일이 존재합니다.");
+        };
+
+        User user = User.builder()
+                .email(userInput.getEmail())
+                .userName(userInput.getUserName())
+                .phone(userInput.getPhone())
+                .password(userInput.getPassword())
+                .regDate(LocalDateTime.now())
+                .build();
+        userRepository.save(user);
+
+        return ResponseEntity.ok().build();
+
+    }
+    */
+    @ExceptionHandler(value = {ExistsEmailException.class, PasswordNotMatchException.class})
+    public ResponseEntity ExistsEmailExceptionHandler(RuntimeException e){
+        return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+
+    @PatchMapping("/api/user/{id}/password")
+    public ResponseEntity updateUserPassword(@PathVariable Long id, @RequestBody UserInputPassword userInputPassword, Errors errors){
+
+        List<ResponseError> responseErrorList = new ArrayList<>();
+        if(errors.hasErrors()){
+            errors.getAllErrors().stream().forEach((e) ->
+                    responseErrorList.add(ResponseError.of((FieldError)e))
+            );
+            return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
+        }
+
+        User user = userRepository.findByIdAndPassword(id, userInputPassword.getPassword())
+                .orElseThrow(()-> new PasswordNotMatchException("비밀번호가 일치하지 않습니다."));
+
+        user.setPassword(userInputPassword.getPassword());
+        userRepository.save(user);
+
+        return ResponseEntity.ok().build();
+
+    }
+
+    private String getEncryptPassword(String password){
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder.encode(password);
+    }
+
+
+    @PostMapping("/api/user")
+    public ResponseEntity addUser(@RequestBody @Valid UserInput userInput, Errors errors){
+
+        List<ResponseError> responseErrorList = new ArrayList<>();
+        if(errors.hasErrors()){
+            errors.getAllErrors().stream().forEach((e) ->
+                    responseErrorList.add(ResponseError.of((FieldError)e))
+            );
+            return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
+        }
+
+        if(userRepository.countByEmail(userInput.getEmail()) > 0) {
+            throw new ExistsEmailException("이미 가입한 이메일이 존재합니다.");
+        };
+
+
+
+        User user = User.builder()
+                .email(userInput.getEmail())
+                .userName(userInput.getUserName())
+                .phone(userInput.getPhone())
+                .password(getEncryptPassword(userInput.getPassword()))
+                .regDate(LocalDateTime.now())
+                .build();
+        userRepository.save(user);
+
+        return ResponseEntity.ok().build();
+
+    }
+
+
+    @DeleteMapping("/api/user/{id}")
+    public ResponseEntity deleteUser(@PathVariable Long id){
+        User user = userRepository.findById(id).orElseThrow(
+                ()->new UserNotFoundException("사용자 정보가 없습니다."));
+
+        // 내가 쓴 공지사항이 있는 경우
+        // -> ??
+        try {
+            userRepository.delete(user);
+        }catch (DataIntegrityViolationException e){
+            String message = "제약조건에 문제가 발생하였습니다.";
+            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+        } catch (Exception e){
+            String message = "회원 탈퇴 중 문제가 발생하였습니다.";
+            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+        }
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/api/user")
+    public ResponseEntity findUser(@RequestBody UserInputFind userInputFind){
+
+        User user = userRepository.findByUserNameAndPhone(userInputFind.getUserName(), userInputFind.getPhone())
+                .orElseThrow(()-> new UserNotFoundException("사용자 정보가 없습니다."));
+
+        UserResponse userResponse = UserResponse.of(user);
+
+        return ResponseEntity.ok().body(userResponse);
+    }
+
+
+
+
+
 
 }
--- SecretCode_SpringBoot-JPA/sample1/src/main/java/com/example/jpa/user/repository/UserRepository.java
+++ SecretCode_SpringBoot-JPA/sample1/src/main/java/com/example/jpa/user/repository/UserRepository.java
@@ -5,9 +5,15 @@ import com.example.jpa.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    int countByEmail(String email);
 
+    Optional<User> findByIdAndPassword(Long id, String password);
+
+    Optional<User> findByUserNameAndPhone(String userName, String phone);
 }
