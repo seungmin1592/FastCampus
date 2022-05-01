8bd5418d58a4906925b36499eb36018fecaf3e04
seungmin1592 <tmdals1592@naver.com>
2022-05-01 23:04:58 +0900

    Java/Spring 웹 개발 - Spring

---
+++ Java&Spring_Web_Dev/validation/src/main/java/com/example/validation/dto/User.java
@@ -0,0 +1,74 @@
+package com.example.validation.dto;
+
+
+import com.example.validation.anotation.YearMonth;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class User {
+
+    @NotBlank
+    private String name;
+
+    @Max(90)
+    private int age;
+
+//    private String email;
+//
+//    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxx(x)-xxxx")
+//    private String phoneNumber;
+//
+//    @YearMonth
+//    private String reqYearMonth; // yyyyMM
+
+    @Valid
+    private List<Car> cars;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+//    @AssertTrue(message = "yyyyMM의 형식에 맞지 않습니다.")
+//    public Boolean isReqYearMonthValidation(){
+//        try{
+//            LocalDate localDate = LocalDate.parse(getReqYearMonth() + "01", DateTimeFormatter.ofPattern("yyyyMMdd"));
+//
+//        } catch (Exception e){
+//            return false;
+//        }
+//        return true;
+//    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", cars=" + cars +
+                '}';
+    }
+}
